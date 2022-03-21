3900381507be7e9b3141d3bc20df52c4524729ff
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-03-20 18:51:05 -0700

    import roles

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 // const fs = requrie('fs');
 const inquirer = require('inquirer');
 
+const Employee = require('./lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
+
 const EmployeeQuestions = require('./lib/EmployeeQuestions.js');
 const EngineerQuestions = require('./lib/EngineerQuestions.js');
 const InternQuestions = require('./lib/InternQuestions.js');
 const ManagerQuestions = require('./lib/ManagerQuestions.js');
 
+// array to contain the team members
+let teamArray = [];
 
 function init() {
     return inquirer.prompt(EmployeeQuestions)
